6dc5af41a6ccd1cd03ae11c4a34871ee2f172158
Jenrick Lim <jenrick_lim@dlsu.edu.ph>
2025-07-03 20:49:38 +0800

    chore(load.c): added comments to functions

--- src/load.c
+++ src/load.c
@@ -1,5 +1,13 @@
 #include "../include/load.h"
 
+/*
+	This function loads the master list of cards from a TXT file into the database.
+	Precondition: The databaseType structure is defined and the master list file exists.
+
+	@param db a pointer to the databaseType structure where the card data will be loaded
+
+	@return 1 if the master list is loaded successfully, otherwise 0
+*/
 int loadMasterList (databaseType *db)
 {
 	FILE* fp = fopen(MASTER_LIST,"r");
@@ -35,10 +43,18 @@ int loadMasterList (databaseType *db)
 	fclose (fp);
 
 	return bSuccess;
-  
 }
 
+/*
+	This function retrieves the card patterns from a file and stores them in the database.
+	Precondition: The databaseType structure is defined and the card file exists.
 
+	@param db a pointer to the databaseType structure where the card patterns will be stored
+	@param filename the name of the file containing the card patterns
+	@param idx the index of the card in the database
+
+	@return 1 if the card patterns are loaded successfully, otherwise 0
+*/
 int getCardPatterns (databaseType *db, String30 filename, int idx)
 {
 	int bSuccess = 1;
@@ -75,7 +91,6 @@ int getCardPatterns (databaseType *db, String30 filename, int idx)
 		}
 	}
 
-
 	for (j = 0; j < SIDE && bSuccess; j++)
 	{
 		if (fscanf(fp,"%5s",buff) != 1)
@@ -98,6 +113,14 @@ int getCardPatterns (databaseType *db, String30 filename, int idx)
 	return bSuccess;
 }
 
+/*
+	This function retrieves the card patterns for all cards in the database from their respective files.
+	Precondition: The databaseType structure is defined and the card files exist.
+
+	@param db a pointer to the databaseType structure where the card patterns will be stored
+
+	@return 1 if all card patterns are loaded successfully, otherwise 0
+*/
 int getCards (databaseType *db)
 {
 	String30 filename;
@@ -115,12 +138,20 @@ int getCards (databaseType *db)
 			printf("Problems with reading %s\n", filename);
 			bSuccess = 0;	
 		}
-		
 	}
 
 	return bSuccess;
 }
 
+/*
+	This function calculates the possible moves for the blue player based on the current card and center position using relative move coordinates.
+	Precondition: The cardType structure is initialized and contains the card data.
+
+	@param card a pointer to the cardType structure containing the card data
+	@param center the center point of the board
+
+	@return this function does not return a value, it updates the moves array in the cardType structure
+*/
 void blueMoves (cardType *card, pointType center)
 {
 	int j, k;
@@ -147,6 +178,14 @@ void blueMoves (cardType *card, pointType center)
 	}	
 }
 
+/*
+	This function calculates the possible moves for the red player based on the blue player's moves by negating the relative move coordinates.
+	Precondition: The cardType structure is initialized and contains the card data.
+
+	@param card a pointer to the cardType structure containing the card data
+
+	@return this function does not return a value, it updates the moves array in the cardType structure
+*/
 void redMoves (cardType *card)
 {
 	int j;
@@ -157,6 +196,15 @@ void redMoves (cardType *card)
 		card->moves[1][j].col = -card->moves[0][j].col;
 	}
 }
+
+/*
+	This function initializes the moves for all cards in the database by calculating the possible moves for each card.
+	Precondition: The databaseType structure is initialized and contains the card data.
+
+	@param db a pointer to the databaseType structure containing the game data
+
+	@return this function does not return a value, it updates the moves for each card in the database
+*/
 void getMoves (cardType *card)
 {
 	pointType center;
@@ -168,6 +216,14 @@ void getMoves (cardType *card)
 	redMoves(card);	
 }
 
+/*
+	This function initializes the moves for all cards in the database by calling getMoves for each card.
+	Precondition: The databaseType structure is initialized and contains the card data.
+
+	@param db a pointer to the databaseType structure containing the game data
+
+	@return this function does not return a value, it updates the moves for each card in the database
+*/
 void initCards (databaseType *db)
 {
 	int j;
@@ -177,6 +233,15 @@ void initCards (databaseType *db)
 		getMoves(&db->cardDb[j]);
 	}
 }
+
+/*
+	This function loads the master list of cards, retrieves the card patterns, and initializes the moves for all cards in the database.
+	Precondition: The databaseType structure is defined and the master list file exists.
+
+	@param db a pointer to the databaseType structure where the card data will be loaded
+
+	@return this function does not return a value, it updates the database with card data
+*/
 void loadCardsFromFile (databaseType *db)
 {
     loadMasterList(db);
