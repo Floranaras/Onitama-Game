9eaf40c78e2dd96d3ada843e3be9e01318bea5fc
Floranaras <carlosranara0@gmail.com>
2025-07-15 15:58:49 +0800

    fix: idk how it got there

--- game.c
+++ game.c
@@ -1,5 +1,4 @@
 #include "check.c"
-#include <stdio.h>
 /*
 	This function initializes the game database with default values, including the board setup, player names, and card database.
 	Precondition: The databaseType structure is defined and the card database is loaded.
